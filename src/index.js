27fd4eccfd102399875414471e97c72e8c9062f3
Tyler Haas <haasdevelopment@gmail.com>
2018-05-26 16:48:57 -0600

    fixed eslint and flow errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -6,10 +7,14 @@ import "./index.css";
 import Routes from "./routes";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
-  document.getElementById("root"),
-);
-registerServiceWorker();
+const root = document.getElementById("root") || null;
+
+if (root) {
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>,
+    root,
+  );
+  registerServiceWorker();
+}
