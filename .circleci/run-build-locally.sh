f863c2d8288fd3c0997d71fcb5d451c91f3b0efe
Tyler Haas <haasdevelopment@gmail.com>
2018-05-29 14:53:09 -0600

    added ability to run circle locally for debugging

---
+++ .circleci/run-build-locally.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl --user ${CIRCLE_TOKEN}: \
+    --request POST \
+    --form revision=$1\
+    --form config=@config.yml \
+    --form notify=false \
+        https://circleci.com/api/v1.1/project/github/tylerthehaas/coinstep/tree/master
