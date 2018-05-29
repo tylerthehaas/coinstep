bebbb2c1ce1968fb503c405510b8b9996d9924b5
Tyler Haas <haasdevelopment@gmail.com>
2018-05-29 16:38:12 -0600

    formatting changes

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -78,23 +78,22 @@ function registerValidSW(swUrl) {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === "installed") {
-            if (
-              navigator &&
-              navigator.serviceWorker &&
-              navigator.serviceWorker.controller
-            ) {
-              // At this point, the old content will have been purged and
-              // the fresh content will have been added to the cache.
-              // It's the perfect time to display a "New content is
-              // available; please refresh." message in your web app.
-              console.log("New content is available; please refresh.");
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log("Content is cached for offline use.");
-            }
+          if (
+            installingWorker.state === "installed" &&
+            navigator &&
+            navigator.serviceWorker &&
+            navigator.serviceWorker.controller
+          ) {
+            // At this point, the old content will have been purged and
+            // the fresh content will have been added to the cache.
+            // It's the perfect time to display a "New content is
+            // available; please refresh." message in your web app.
+            console.info("New content is available; please refresh.");
+          } else {
+            // At this point, everything has been precached.
+            // It's the perfect time to display a
+            // "Content is cached for offline use." message.
+            console.info("Content is cached for offline use.");
           }
         };
       };
@@ -127,7 +126,7 @@ function checkValidServiceWorker(swUrl) {
       }
     })
     .catch(() => {
-      console.log(
+      console.info(
         "No internet connection found. App is running in offline mode.",
       );
     });
